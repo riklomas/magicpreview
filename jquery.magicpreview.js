8656f4238527f63a3cbb0a40b171b941c17d6905
Rik Lomas <lomas.rik@gmail.com>
2008-11-14 17:31:48 +0000

    Added checkboxes, radios and selects

--- jquery.magicpreview.js
+++ jquery.magicpreview.js
@@ -1,14 +1,39 @@
 ;(function( $ ){
 	$.fn.magicpreview = function(str, options) {
-		
 		return this.filter(':text, :radio, :checkbox, select, textarea').each(function () {
-			var n = '#' + str + $(this).attr('id');
+			var n = '#' + str + $(this).attr('name');
 			var o = $(n).text();
-			$(this).keyup(function () {
-				var st = ($(this).val().replace(/\n|\r/mg, '') != '') ? $(this).val() : o;
-				( $(this).is('textarea') ) ? $(n).html(st.replace(/\r|\n/mg, '<br />')) : $(n).text(st);
-			});
-			$(this).trigger('keyup');
+			
+			if ($(this).is(':text, textarea'))
+			{
+				$(this).keyup(function () {
+					var st = ($(this).val().replace(/\n|\r/mg, '') != '') ? $(this).val() : o;
+					( $(this).is('textarea') ) ? $(n).html(st.replace(/\r|\n/mg, '<br />')) : $(n).text(st);
+				});
+				$(this).trigger('keyup');
+			}
+			else if ($(this).is(':checkbox, :radio'))
+			{
+				function dothis(e)
+				{
+					$(n).html( ($(e).is(':checked')) ? $(e).val() : o );
+				}
+				$(this).click(function () { 
+					dothis(this);
+				});
+				dothis(this);
+			}
+			else if ($(this).is('select'))
+			{
+				function dothis(e)
+				{
+					$(n).html($(e).val());
+				}
+				$(this).change(function () {
+					dothis(this);
+				})
+				dothis(this);
+			}
 		});
 	}
 })( jQuery );
