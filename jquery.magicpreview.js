ad65f2f894f1f0bda07f20ce6572c977c36704e9
Rik <lomas.rik@gmail.com>
2008-11-15 12:29:27 +0000

    Rearranged structure and added onBefore and onAfter

--- jquery.magicpreview.js
+++ jquery.magicpreview.js
@@ -1,39 +1,62 @@
 ;(function( $ ){
 	$.fn.magicpreview = function(str, options) {
+		
 		return this.filter(':text, :radio, :checkbox, select, textarea').each(function () {
+			
 			var n = '#' + str + $(this).attr('name');
 			var o = $(n).text();
 			
 			if ($(this).is(':text, textarea'))
 			{
 				$(this).keyup(function () {
-					var st = ($(this).val().replace(/\n|\r/mg, '') != '') ? $(this).val() : o;
-					( $(this).is('textarea') ) ? $(n).html(st.replace(/\r|\n/mg, '<br />')) : $(n).text(st);
+					$.fn.magicpreview.change(this, n, o, 'text', false);
 				});
-				$(this).trigger('keyup');
+				$.fn.magicpreview.change(this, n, o, 'text', true);
 			}
 			else if ($(this).is(':checkbox, :radio'))
 			{
-				function dothis(e)
-				{
-					$(n).html( ($(e).is(':checked')) ? $(e).val() : o );
-				}
 				$(this).click(function () { 
-					dothis(this);
+					$.fn.magicpreview.change(this, n, o, 'check', false);
 				});
-				dothis(this);
+				$.fn.magicpreview.change(this, n, o, 'check', true);
 			}
 			else if ($(this).is('select'))
 			{
-				function dothis(e)
-				{
-					$(n).html($(e).val());
-				}
 				$(this).change(function () {
-					dothis(this);
+					$.fn.magicpreview.change(this, n, o, 'select', false);
 				})
-				dothis(this);
+				$.fn.magicpreview.change(this, n, o, 'select', true);
 			}
 		});
+	};
+	
+	$.fn.magicpreview.change = function (e, n, o, typ, onload)
+	{
+		if ($.fn.magicpreview.options.onBefore() && ($.fn.magicpreview.onLoad || !onload))
+		{
+			var st = $.fn.magicpreview.changers[typ](e, o);
+			$(n).html(st);
+			$.fn.magicpreview.options.onAfter()
+		}
+	};
+	
+	$.fn.magicpreview.changers = {
+		'text': function (e, o) {
+			var st = ($(e).val().replace(/\n|\r/mg, '') != '') ? $(e).val() : o;
+			st = ( $(e).is('textarea') ) ? st.replace(/\r|\n/mg, '<br />') : st;
+			return st;
+		},
+		'check': function (e, o) {
+			return ($(e).is(':checked')) ? $(e).val() : o ;
+		},
+		'select': function (e, o) {
+			return $(e).val();
+		}
 	}
+	
+	$.fn.magicpreview.options = {
+		'onLoad': true,
+		'onBefore': function () { return true; },
+		'onAfter': function () { return true; }
+	};
 })( jQuery );
